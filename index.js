c1049663ef62920f2e6e7f8172417fb4617a9781
Your Name <you@example.com>
2022-08-31 22:05:35 -0400

    fix fix fix

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ const menuToggle = document.querySelector('.list-link')
 
 navMenu.addEventListener('click', (e) => {
     e.preventDefault();
-    menuToggle.classList.toggle('menu-toggle')
+    menuToggle.classList.toggle('menu-toggle');
 })
+
+
+
